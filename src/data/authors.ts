3e4d20a30091083bd12e6d5995047b8d8f308c90
oxonomy14 <oxonomy14@gmail.com>
2026-08-23 22:39:04 +0700

    add article

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -10,6 +10,15 @@ export const authors = {
     image: '/images/author/pavel-dementiev-article.webp',
     href: '/about',
   },
+  'andrei-semenenko': {
+    id: 'andrei-semenenko',
+    name: 'Андрей Семененко',
+    role: 'Администратор InGenium Life',
+    description:
+      'Выпускник InGenium, прошёл полный цикл обучения натальной астрологии и получил сертификат по результатам итогового экзамена.',
+    image: '/images/author/andrei-semenenko-article.webp',
+    href: '',
+  },
 } as const;
 
 export type AuthorId = keyof typeof authors;
